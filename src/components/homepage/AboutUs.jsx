7a449c81ce52f98e9b68ca3ef2461fe0d017d4d9
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-07-29 23:04:05 +0600

    added about section

--- src/components/homepage/AboutUs.jsx
+++ src/components/homepage/AboutUs.jsx
@@ -1,23 +1,52 @@
+import Image from "next/image";
+import img1 from "../../../public/assets/about section/about-us-1.webp";
+import img2 from "../../../public/assets/about section/about-us-2.webp";
+import img3 from "../../../public/assets/about section/about-us-3.webp";
+import img4 from "../../../public/assets/about section/about-us-4.webp";
+import { PiStarFourFill } from "react-icons/pi";
+import { GiAutoRepair } from "react-icons/gi";
+import { ImStopwatch } from "react-icons/im";
+import { FaScrewdriverWrench } from "react-icons/fa6";
+import { FaPeopleCarry } from "react-icons/fa";
 
 
 const AboutUs = () => {
     return (
         <div>
             <section className="dark:bg-gray-100 dark:text-gray-800">
-	<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-		<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-			<img src="assets/svg/Business_SVG.svg" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+	<div className="container flex  gap-6 flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+		<div className="grid grid-cols-2 border border-red-600 items-center justify-center ">
+			
+			<Image src={img1} alt="" width="400" height="400" className="rounded-r-full"></Image>
+			<Image src={img2} alt="" width="400" height="400" className="rounded-l-full"></Image>
+			<Image src={img3} alt="" width="400" height="400" className="rounded-r-full"></Image>
+			<Image src={img4} alt="" width="400" height="400" className="rounded-l-full"></Image>
 		</div>
-		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-			<h1 className="text-5xl font-bold leading-none sm:text-6xl">Ac mattis
-				<span className="dark:text-violet-600">senectus</span>erat pharetra
+		<div className="flex w-3/5 border border-green-600 flex-col justify-center p-6 text-center rounded-sm  lg:text-left">
+		<h6 className="border flex items-center text-orange-600 pb-4"><PiStarFourFill className="" />ABOUT US
+		<PiStarFourFill  /></h6>
+			<h1 className="text-4xl font-bold leading-none sm:text-5xl">Empowering Your Devices:
+				<span className="dark:text-violet-600"> Crafting Solutions</span>
 			</h1>
-			<p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
+			<p className="mt-6 mb-8 text-lg sm:mb-12">Welcome to PC Repair Pros, where technology meets expertise. With a passion for problem-solving and a dedication to exceptional service
 				<br  className="hidden md:inline lg:hidden" />turpis pulvinar, est scelerisque ligula sem
 			</p>
-			<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Suspendisse</a>
-				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-800">Malesuada</a>
+			<div className="grid grid-cols-2 gap-6 mb-10">
+				<p className="flex font-bold items-center gap-2">
+				<FaScrewdriverWrench className="text-3xl text-orange-600 bg-orange-100 w-16 h-16 p-2 rounded-lg"/>Expert Technicians
+				</p>
+				<p className="flex font-bold items-center gap-2">
+				<GiAutoRepair className="text-3xl text-blue-700 bg-blue-100 w-16 h-16 p-2 rounded-lg"/>Quality Repairs
+				</p>
+				<p className="flex font-bold items-center gap-2">
+				<ImStopwatch className="text-3xl text-blue-700 bg-blue-100 w-16 h-16 p-2 rounded-lg"/>Quick Turnaround
+				</p>
+				<p className="flex font-bold items-center gap-2">
+				<FaPeopleCarry className="text-3xl text-orange-600  bg-orange-100 w-16 h-16 p-2 rounded-lg"/>Expert Technicians
+				</p>
+			</div>
+			<div>
+				<p>Our mission is to provide reliable, efficient, and affordable repair services, ensuring that your devices are restored to optimal functionality. We understand the vital role technology plays in your daily life, and we are committed to keeping you connected.</p>
 			</div>
 		</div>
 	</div>
